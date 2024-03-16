4eb334deb3c1ebe515832a9a56af9e234599cfcf
Sajay Prakash <sajayprakashk@gmail.com>
2024-03-16 19:27:49 +0530

    add footer flags and languages

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,7 +1,51 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export function Footer() {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex  items-center justify-evenly h-full">
+        <Button variant="ghost" size="lg" className="w-full">
+          <Image
+            src="/fr-flag.svg"
+            alt="French"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button variant="ghost" size="lg" className="w-full">
+          <Image
+            src="/in-flag.svg"
+            alt="Tamil"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Tamil
+        </Button>
+        <Button variant="ghost" size="lg" className="w-full">
+          <Image
+            src="/in-flag.svg"
+            alt="Hindi"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Hindi
+        </Button>
+        <Button variant="ghost" size="lg" className="w-full">
+          <Image
+            src="/jp-flag.svg"
+            alt="Japanese"
+            width={40}
+            height={32}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+      </div>
     </footer>
   );
 }
