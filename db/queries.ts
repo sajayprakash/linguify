78bdaafdfa3704f986960b5222809be6d64686b5
Sajay Prakash <sajayprakashk@gmail.com>
2024-07-26 10:23:47 +0530

    add check condition if lesson does not have challenges

--- db/queries.ts
+++ db/queries.ts
@@ -59,6 +59,9 @@ export const getUnits = cache(async () => {
   });
   const normalizedData = data.map((unit) => {
     const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
       const allCompletedChalleneges = lesson.challenges.every((challenge) => {
         return (
           challenge.challengeProgress &&
